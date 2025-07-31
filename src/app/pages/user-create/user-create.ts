5ff197ec85dec6ff565fc0b4e85dff92f52e89a7
Dmitry <dims3@mail.ru>
2025-07-31 18:26:30 +0500

    переделка под новую навигацию.

--- src/app/pages/user-create/user-create.ts
+++ src/app/pages/user-create/user-create.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Select } from 'primeng/select';
 import { InputText } from 'primeng/inputtext';
@@ -9,105 +9,53 @@ import { ButtonModule } from 'primeng/button';
 import { MessageModule } from 'primeng/message';
 import { InputGroupModule } from 'primeng/inputgroup';
 import { RippleModule } from 'primeng/ripple';
+import { RadioButton } from 'primeng/radiobutton';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-user-create',
     standalone: true,
-    imports: [Select, InputText, TextareaModule, FileUploadModule, ButtonModule, InputGroupModule, RippleModule, MessageModule],
-    template: `<div class="card">
-        <span class="text-surface-900 dark:text-surface-0 text-xl font-bold mb-6 block">Данные профиля</span>
-        <div class="grid-cols-12 gap-4">
-            <div class="col-span-12 lg:col-span-2">
-                <p class="m-0 p-0 text-red-600 dark:text-surface-200 leading-normal mr-4">Заполние все поля.</p>
-            </div>
-            <div class="col-span-12 lg:col-span-10">
-                <div class="grid grid-cols-12 gap-4">
-                    <div class="mb-6 col-span-12">
-                        <label for="nickname" class="font-medium text-surface-900 dark:text-surface-0 mb-2 block"> Ф.И.О </label>
-                        <input id="nickname" type="text" pInputText fluid />
-                    </div>
-                    <div class="mb-6 col-span-12 flex flex-col items-start">
-                        <label for="avatar" class="font-medium text-surface-900 dark:text-surface-0 mb-2 block">Аватар</label>
-                        <p-fileupload mode="basic" name="avatar" url="./upload.php" accept="image/*" [maxFileSize]="1000000" styleClass="p-button-outlined p-button-plain" chooseLabel="Загрузить фото"></p-fileupload>
-                    </div>
-                    <div class="mb-6 col-span-12">
-                        <label for="bio" class="font-medium text-surface-900 dark:text-surface-0 mb-2 block"> Наименование компании </label>
-                        <input pTextarea id="bio" type="text" rows="5" [autoResize]="true" fluid />
-                    </div>
-                    <div class="mb-6 col-span-12 md:col-span-6">
-                        <label for="email" class="font-medium text-surface-900 dark:text-surface-0 mb-2 block"> Email </label>
-                        <input id="email" type="text" pInputText fluid />
-                    </div>
-                    <div class="mb-6 col-span-12 md:col-span-6">
-                        <label for="country" class="font-medium text-surface-900 dark:text-surface-0 mb-2 block"> Город </label>
-                        <p-select inputId="country" [options]="countries" optionLabel="name" fluid [filter]="true" filterBy="name" [showClear]="true" placeholder="Выберете город">
-                            <ng-template let-country #item>
-                                <div class="flex items-center">
-                                    <img src="https://primefaces.org/cdn/v2/images/flag/flag_placeholder.png" [class]="'mr-2 flag flag-' + country.code.toLowerCase()" style="width:18px" />
-                                    <div>{{ country.name }}</div>
-                                </div>
-                            </ng-template>
-                        </p-select>
-                    </div>
-                    <div class="mb-6 col-span-12 md:col-span-6">
-                        <label for="city" class="font-medium text-surface-900 dark:text-surface-0 mb-2 block"> БИН </label>
-                        <input id="city" type="text" pInputText fluid />
-                    </div>
-                    <div class="mb-6 col-span-12 md:col-span-6">
-<!--                        <label for="state" class="font-medium text-surface-900 dark:text-surface-0 mb-2 block"> ИИН </label>-->
-<!--                        <input id="state" type="text" pInputText fluid />-->
-                    </div>
-<!--                    <div class="mb-6 col-span-12">-->
-<!--                        <label for="website" class="font-medium text-surface-900 dark:text-surface-0 mb-2 block"> Website </label>-->
-<!--                        <p-inputgroup>-->
-<!--                            <p-inputgroup-addon>-->
-<!--                                <span>www</span>-->
-<!--                            </p-inputgroup-addon>-->
-<!--                            <input id="website" type="text" pInputText fluid />-->
-<!--                        </p-inputgroup>-->
-<!--                    </div>-->
-                    <div class="flex" style="width: 450%">
-                        <div class="col-span-12 mx-5">
-                            <button pButton (click)="onSubmit()" pRipple label="Сохранить" class="w-auto mt-3"></button>
-                        </div>
-                        <div class="col-span-12" style="width: 90%">
-                            <button pButton (click)="backToLogin()" pRipple label="Вернуться на страницу авторизации" class="w-auto mt-3"></button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div> `
+    imports: [Select, InputText, TextareaModule, FileUploadModule, CommonModule, ButtonModule, InputGroupModule, RippleModule, MessageModule, RadioButton, FormsModule, ReactiveFormsModule],
+    templateUrl: './user-create.component.html'
 })
 export class UserCreate {
-    countries: any[] = [];
-    loginForm: FormGroup;
+    city: any[] = [];
+    avatar: any;
+    createForm: FormGroup;
+    activityTypes: any[] = [];
+
     constructor(private fb: FormBuilder, private router: Router,) {
-        this.loginForm = this.fb.group({
+        this.createForm = this.fb.group({
             email: [''],
-            password: [''],
-            remember: [false]
+            userName: [''],
+            company: [''],
+            avatar: [''],
+            ingredient: [0, Validators.required]
         });
     }
 
-
     ngOnInit() {
-        this.countries = [
-            { name: 'Australia', code: 'AU' },
-            { name: 'Brazil', code: 'BR' },
-            { name: 'China', code: 'CN' },
-            { name: 'Egypt', code: 'EG' },
-            { name: 'France', code: 'FR' },
-            { name: 'Germany', code: 'DE' },
-            { name: 'India', code: 'IN' },
-            { name: 'Japan', code: 'JP' },
-            { name: 'Spain', code: 'ES' },
-            { name: 'United States', code: 'US' }
+        this.city = [
+            { name: 'Астана', code: '1' },
+            { name: 'Алмата', code: '2' },
+            { name: 'Шимкент', code: '3' },
+            { name: 'Петропавловск', code: '4' },
+            { name: 'Костанай', code: '5' },
+            { name: 'Усть Каменогорск', code: '6' },
+            { name: 'Щучинск', code: '7' },
+            { name: 'Караганда', code: '8' },
+            { name: 'Уральск', code: '9' },
+            { name: 'Кокшетау', code: '10' }
+    ];
+        this. activityTypes = [
+            { label: 'Физ.лицо', value: 1 },
+            { label: 'ИП', value: 2 },
+            { label: 'ТОО', value: 3 }
         ];
     }
     onSubmit() {
-        if (this.loginForm.valid) {
-            this.router.navigate(['/dashboard']);
+        if (this.createForm.valid) {
+            this.router.navigate(['/my-orders']);
         }
     }
     backToLogin() {
